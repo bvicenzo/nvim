7de8aebbb3347947ec87444cc813d7d484ceb607
Bruno Vicenzo <bruno.vicenzo@fretadao.com.br>
2023-04-30 16:58:31 -0300

    Add Lazy plugin management and Mini Plugin to test

--- init.lua
+++ init.lua
@@ -1,6 +1,24 @@
 -- Author: Bruno Vicenzo
 -- Source: https://github.com/bvicenzo/nvim.git
 
+-- Lazy - Package manager
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+
+-- Lazy load plugins
+require("lazy").setup("plugins")
+
 -- Vim custom options
 vim.g.mapleader = ","           -- change the <leader> to comma(,)
 vim.keymap.set('n', ';', ':')   -- don't need to press the shift key :
